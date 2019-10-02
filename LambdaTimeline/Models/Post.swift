e0a9f43244242f457a1b486fc36db0ef493cb092
Marlon <marmoneymar@icloud.com>
2019-10-02 01:20:01 -0400

    Added audioURL to Comments object and started changing Post object

--- LambdaTimeline/Models/Post.swift
+++ LambdaTimeline/Models/Post.swift
@@ -14,14 +14,36 @@ enum MediaType: String {
 }
 
 class Post {
+
+    var mediaURL: URL
+    let mediaType: MediaType
+    let author: Author
+    let timestamp: Date
+    var comments: [Comment]
+    var id: String?
+    var ratio: CGFloat?
+
+    var title: String? {
+        return comments.first?.text
+    }
+
+    static private let mediaKey = "media"
+    static private let ratioKey = "ratio"
+    static private let mediaTypeKey = "mediaType"
+    static private let authorKey = "author"
+    static private let commentsKey = "comments"
+    static private let timestampKey = "timestamp"
+    static private let idKey = "id"
     
-    init(title: String, mediaURL: URL, ratio: CGFloat? = nil, author: Author, timestamp: Date = Date()) {
+    init(title: String, audioURL: URL, mediaURL: URL, ratio: CGFloat? = nil, author: Author, timestamp: Date = Date()) {
         self.mediaURL = mediaURL
         self.ratio = ratio
         self.mediaType = .image
         self.author = author
-        self.comments = [Comment(text: title, author: author)]
+        self.comments = [Comment(text: title, audioURL: audioURL, author: author)]
         self.timestamp = timestamp
+
+        #warning("Ask about this tomorrow")
     }
     
     init?(dictionary: [String : Any], id: String) {
@@ -56,24 +78,5 @@ class Post {
         
         return dict
     }
-    
-    var mediaURL: URL
-    let mediaType: MediaType
-    let author: Author
-    let timestamp: Date
-    var comments: [Comment]
-    var id: String?
-    var ratio: CGFloat?
-    
-    var title: String? {
-        return comments.first?.text
-    }
-    
-    static private let mediaKey = "media"
-    static private let ratioKey = "ratio"
-    static private let mediaTypeKey = "mediaType"
-    static private let authorKey = "author"
-    static private let commentsKey = "comments"
-    static private let timestampKey = "timestamp"
-    static private let idKey = "id"
+
 }
